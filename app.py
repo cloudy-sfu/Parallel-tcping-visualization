3fb8fcd74cdfd0d3da230a1afda01ab72c24d022
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2023-08-21 17:00:36 +1200

    v1.2

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ import pytz
 import socket
 import time
 import webbrowser
+import atexit
 
 
 class TCPing(Thread):
-    def __init__(self, hostname, db_path, table_name, max_ping=None):
+    def __init__(self, hostname, db_path, max_ping=None):
         super(TCPing, self).__init__()
         self.db_path = db_path
-        self.table_name = table_name
         self.stop_query = Event()
         self.hostname = hostname
         self.iterator = range(max_ping + 2) if max_ping else count()
@@ -48,10 +48,10 @@ class TCPing(Thread):
                 now_1 = datetime.now(tz=pytz.utc)
                 s.close()
                 delay = (now_1 - now_0) / timedelta(milliseconds=1)
-                c.execute(f"INSERT INTO {self.table_name} (hostname, delay) VALUES (?, ?)",
+                c.execute(f"INSERT INTO tcping (hostname, delay) VALUES (?, ?)",
                           (self.hostname, delay))
             except (socket.timeout, OSError):
-                c.execute(f"INSERT INTO {self.table_name} (hostname, delay) VALUES (?, ?)",
+                c.execute(f"INSERT INTO tcping (hostname, delay) VALUES (?, ?)",
                           (self.hostname, None))
             connection.commit()
             c.close()
@@ -63,12 +63,11 @@ class Ping(Thread):
     """
     Deprecated. The source code is kept as a fallback in case TCPing has problems.
     """
-    def __init__(self, hostname, db_path, table_name, max_ping=None):
+    def __init__(self, hostname, db_path, max_ping=None):
         super(Ping, self).__init__()
         self.hostname = hostname
         self.iterator = range(max_ping + 2) if max_ping else count()
         self.db_path = db_path
-        self.table_name = table_name
         self.stop_query = Event()
 
     def run(self):
@@ -83,10 +82,10 @@ class Ping(Thread):
                 delay_number = re.search(r"time=(\d*)ms", delay_text)
                 if delay_number:
                     delay_ms = int(delay_number.group(1))
-                    c.execute(f"INSERT INTO {self.table_name} (hostname, delay) VALUES (?, ?)",
+                    c.execute(f"INSERT INTO tcping (hostname, delay) VALUES (?, ?)",
                               (self.hostname, delay_ms))
                 elif delay_text == "Request timed out.":
-                    c.execute(f"INSERT INTO {self.table_name} (hostname, delay) VALUES (?, ?)",
+                    c.execute(f"INSERT INTO tcping (hostname, delay) VALUES (?, ?)",
                               (self.hostname, None))
                 connection.commit()
                 c.close()
@@ -96,17 +95,17 @@ class Ping(Thread):
 
 
 class PingController:
-    def __init__(self, db_path, task_name):
+    def __init__(self, db_path):
         self.db_path = db_path
         self.threads = {}
-        self.task_name = task_name
+        # self.task_name = task_name
         connection = sqlite3.connect(self.db_path)
         c = connection.cursor()
-        c.execute(f"CREATE TABLE IF NOT EXISTS {self.task_name} (t TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
-                  f"hostname text, delay float)")
-        c.execute(f'CREATE INDEX IF NOT EXISTS t_idx ON {self.task_name} (t)')
+        c.execute("CREATE TABLE IF NOT EXISTS tcping (t TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+                  "hostname text, delay float)")
+        c.execute('CREATE INDEX IF NOT EXISTS t_idx ON tcping (t)')
         connection.commit()
-        c.execute(f'SELECT DISTINCT hostname FROM {self.task_name}')
+        c.execute('SELECT DISTINCT hostname FROM tcping')
         for x in c.fetchall():
             self.threads[x[0]] = None
         c.close()
@@ -118,7 +117,7 @@ class PingController:
         t = self.threads.get(hostname)
         if isinstance(t, TCPing) and t.is_alive():
             return
-        t = TCPing(hostname, self.db_path, self.task_name)
+        t = TCPing(hostname, self.db_path)
         self.threads[hostname] = t
         t.start()
 
@@ -133,50 +132,23 @@ class PingController:
 
 
 app = Flask(__name__)
-database_path = 'ping.db'
-task = None
-# Why, unlike cloudy-sfu/Homework-grader, do I use global variable "task", instead of recognizing the task by its name
-# in "GET" arguments?
-# Because "task" has threads. I expect only one pool is actively testing delays at the same time. If the user switch
-# to another task, all threads in the previous task should be closed.
+database_path = 'tcping.db'
+task = PingController(db_path=database_path)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    connection = sqlite3.connect(database_path)
-    c = connection.cursor()
-    c.execute("SELECT name FROM sqlite_schema WHERE type='table'")
-    pools = [x[0] for x in c.fetchall()]
-    return render_template('index.html', pools=pools)
-
-
-@app.route('/task', methods=['GET'])
-def view_task():
-    global task
-    if not isinstance(task, PingController):
-        return '&leftarrow; Click the button to start with.'
-    hosts = task.threads.keys()
+    hosts = list(task.threads.keys())
     return render_template('task.html', hosts=hosts)
 
 
-@app.route('/choose-task', methods=['POST'])
-def choose_task():
-    global task
-    if isinstance(task, PingController):
-        for host in task.threads.keys():
-            task.stop(host)
-        task = None
-    new_task_name = request.form.get('table')
-    if new_task_name:
-        task = PingController(db_path=database_path, task_name=new_task_name)
-    return redirect('/')
+def close_all_tasks():
+    for host in task.threads.keys():
+        task.stop(host)
 
 
 @app.route('/data', methods=['POST'])  # AJAX
 def request_data():
-    global task
-    if not isinstance(task, PingController):
-        return jsonify({'error': 'The data table isn\'t assigned, please choose the data table.'})
     hostname = request.form.get('host')
     if not hostname:
         return jsonify({'error': 'Hostname isn\'t defined.'})
@@ -186,10 +158,10 @@ def request_data():
     c = connection.cursor()
     last_fetched = request.form.get('last_fetched')
     if last_fetched:  # TODO: database set datetime index
-        c.execute(f"SELECT t, delay, iif(delay is null, 1, null) FROM {task.task_name} WHERE t > ? AND hostname = ?",
+        c.execute(f"SELECT t, delay, iif(delay is null, 1, null) FROM tcping WHERE t > ? AND hostname = ?",
                   (last_fetched, hostname))
     else:
-        c.execute(f"SELECT t, delay, iif(delay is null, 1, null) FROM {task.task_name} WHERE hostname = ?", (hostname,))
+        c.execute(f"SELECT t, delay, iif(delay is null, 1, null) FROM tcping WHERE hostname = ?", (hostname,))
     records = list(zip(*c.fetchall()))
     c.close()
     if len(records) == 3:
@@ -202,9 +174,6 @@ def request_data():
 
 @app.route('/threads', methods=['POST'])  # AJAX
 def control_threads():
-    global task
-    if not isinstance(task, PingController):
-        return jsonify({'error': 'The data table isn\'t assigned, please choose the data table.'})
     host = request.form.get('host')
     action = request.form.get('action')
     match action:
@@ -226,7 +195,7 @@ def control_threads():
                 del task.threads[host]
                 connection = sqlite3.connect(task.db_path)
                 c = connection.cursor()
-                c.execute(f"DELETE FROM {task.task_name} WHERE hostname = ?", (host,))
+                c.execute(f"DELETE FROM tcping WHERE hostname = ?", (host,))
                 connection.commit()
                 c.close()
         case _:
@@ -236,4 +205,5 @@ def control_threads():
 
 if __name__ == '__main__':
     webbrowser.open_new_tab('http://localhost:5000')
+    atexit.register(close_all_tasks)
     app.run()
